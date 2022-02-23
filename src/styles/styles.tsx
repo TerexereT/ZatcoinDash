6595c23c9714d4e63bf2a5d03cb792e1245affce
Armando Rivas <armandojrivasc@gmail.com>
2022-02-22 21:17:04 -0400

    color button fixed

--- src/styles/styles.tsx
+++ src/styles/styles.tsx
@@ -13,34 +13,34 @@ export const stylesS = {
 };
 
 export const stylesDef = {
-	background: '#053D57',
-	'-webkit-box-shadow': '0px 10px 20px 0px #053D57',
-	boxShadow: '0px 10px 20px 0px #053D57',
+	background: '#005278',
+	'-webkit-box-shadow': '0px 10px 20px 0px #005278',
+	boxShadow: '0px 10px 20px 0px #005278',
 	'&:hover': {
-		background: '#005278',
-		'-webkit-box-shadow': '0px 10px 20px 0px #005278',
-		boxShadow: '0px 10px 20px 0px #005278',
+		background: '#053D57',
+		'-webkit-box-shadow': '0px 10px 20px 0px #053D57',
+		boxShadow: '0px 10px 20px 0px #053D57',
 	},
 } as const;
 
 export const stylesSuccess = {
-	background: '#16A34A',
-	'-webkit-box-shadow': '0px 10px 20px 0px #16A34A',
-	boxShadow: '0px 10px 20px 0px #16A34A',
+	background: '#22C55E',
+	'-webkit-box-shadow': '0px 10px 20px 0px #22C55E',
+	boxShadow: '0px 10px 20px 0px #22C55E',
 	'&:hover': {
-		background: '#22C55E',
-		'-webkit-box-shadow': '0px 10px 20px 0px #22C55E',
-		boxShadow: '0px 10px 20px 0px #22C55E',
+		background: '#16A34A',
+		'-webkit-box-shadow': '0px 10px 20px 0px #16A34A',
+		boxShadow: '0px 10px 20px 0px #16A34A',
 	},
 } as const;
 
 export const stylesError = {
-	background: '#BE123C',
-	'-webkit-box-shadow': '0px 10px 20px 0px #be123c',
-	boxShadow: '0px 10px 20px 0px #be123c',
+	background: '#E11D48',
+	'-webkit-box-shadow': '0px 10px 20px 0px #E11D48',
+	boxShadow: '0px 10px 20px 0px #E11D48',
 	'&:hover': {
-		'-webkit-box-shadow': '0px 10px 20px 0px #E11D48',
-		boxShadow: '0px 10px 20px 0px #E11D48',
-		background: '#E11D48',
+		background: '#BE123C',
+		'-webkit-box-shadow': '0px 10px 20px 0px #BE123C',
+		boxShadow: '0px 10px 20px 0px #BE123C',
 	},
 } as const;
