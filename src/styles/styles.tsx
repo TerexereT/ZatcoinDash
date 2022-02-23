12259ae05ef79b4c41fe2adbceeb3eb2fcbc0d09
Armando Rivas <armandojrivasc@gmail.com>
2022-02-22 21:08:25 -0400

    boxshadows

--- src/styles/styles.tsx
+++ src/styles/styles.tsx
@@ -14,21 +14,33 @@ export const stylesS = {
 
 export const stylesDef = {
 	background: '#053D57',
+	'-webkit-box-shadow': '0px 5px 15px 2px #053D57',
+	boxShadow: '0px 5px 15px 2px #053D57',
 	'&:hover': {
 		background: '#005278',
+		'-webkit-box-shadow': '0px 5px 15px 2px #005278',
+		boxShadow: '0px 5px 15px 2px #005278',
 	},
 } as const;
 
 export const stylesSuccess = {
 	background: '#16A34A',
+	'-webkit-box-shadow': '0px 5px 15px 2px #16A34A',
+	boxShadow: '0px 5px 15px 2px #16A34A',
 	'&:hover': {
 		background: '#22C55E',
+		'-webkit-box-shadow': '0px 5px 15px 2px #22C55E',
+		boxShadow: '0px 5px 15px 2px #22C55E',
 	},
 } as const;
 
 export const stylesError = {
 	background: '#BE123C',
+	'-webkit-box-shadow': '0px 5px 15px 2px #be123c',
+	boxShadow: '0px 5px 15px 2px #be123c',
 	'&:hover': {
+		'-webkit-box-shadow': '0px 5px 15px 2px #E11D48',
+		boxShadow: '0px 5px 15px 2px #E11D48',
 		background: '#E11D48',
 	},
 } as const;
